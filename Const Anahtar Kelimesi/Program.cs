46a5a2590239cf1ae9ccc87e25d5866d4571f8a8
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-11 22:22:28 +0300

    Proje dosyası ekle.

---
+++ Const Anahtar Kelimesi/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Const_Anahtar_Kelimesi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //const sabit tanımlamalar için kullanılır
+            const double PI = 3.14;
+            Console.WriteLine(PI);
+            Console.ReadKey();
+        }
+    }
+}
